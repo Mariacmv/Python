6d2630db94bab6405e30ab3c8c0d15346b102b05
Mariacmv <135616880+Mariacmv@users.noreply.github.com>
2025-03-28 17:23:17 -0300

    Movendo main2.py

---
+++ exercicio/main2.py
@@ -0,0 +1,10 @@
+#5:
+# Em um arquivo chamado main.py, importe a classe Carro.
+from exercicios_aula9 import Carro
+#6:
+# No arquivo main.py, instancie três objetos da classe Carro com 
+# diferentes características, como marca, modelo e cor.
+carro_a = Carro('Chevrolet', 'Onix', 'Preto')
+carro_b = Carro('Ford', 'Bronco Sport', 'Branco')
+
+print(carro_a,'\n',carro_b)
